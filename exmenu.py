715235f84d96af4352b757efb5bc02d3eaf85242
Laion <laionjordy07@hotmail.com>
2021-04-18 20:07:15 -0300

    commit com o menu

---
+++ exmenu.py
@@ -0,0 +1,9 @@
+from ex1 import inversa
+from ex2 import qtd_palavras
+from ex3 import iniciais
+from ex4 import alfabeto
+
+inversa('ONIBUS')
+qtd_palavras('O rato roeu a roupa do rei de roma')
+iniciais('O rato roeu a roupa do rei de roma')
+alfabeto('O rato roeu a roupa do rei de roma')
